602cf35ece5c6e38418ec854431252a1b90f6550
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-04-10 22:16:38 -0300

    login validation rule

---
+++ _module/validateLogin_modu.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+//USUARIOS DO SISTEMA
+$users = array(
+    array("email" => "adm@teste.com.br", "password" => "123456"),
+    array("email" => "user@teste.com.br", "password" => "abcd")
+);
+
+// var_dump($users);
+
+foreach ($users as ["email" => $user, "password" => $pass]) {
+    if ($_POST["email"] == $user && $_POST["senha"] == $pass) {
+        $_SESSION["autentication"] = true;
+        header("Location: home.php");
+        break;
+
+    } else {
+        $_SESSION["autentication"] = false;
+        header("Location: index.php?login=erro");
+    }
+}
+
+
+
+
+
