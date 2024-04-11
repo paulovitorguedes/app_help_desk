c3f9c01692d758de44baa93c338ed13257360f52
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-04-10 21:37:34 -0300

    home.php ticket page and ticket search

---
+++ home.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once "valida_session.php";
+
+?>
+
+
+<html>
+
+
+<head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="_css/home_css.css">
+    <link rel="stylesheet" href="_css/style.css">
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="home.php">
+            <img src="_img/logo.png" class="d-inline-block align-top" alt="logo.pmg">
+            App Help Desk
+        </a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="logoff.php">SAIR</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="card-home">
+                <div class="card">
+                    <div class="card-header">
+                        Menu
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-6 d-flex justify-content-center">
+                                <a href="abrir_chamado.php">
+                                    <img id="img_edita" src="_img/formulario_abrir_chamado.png"
+                                        alt="formulario_abrir_chamado.png">
+                                </a>
+                            </div>
+                            <div class="col-6 d-flex justify-content-center">
+                                <a href="consultar_chamado.php">
+                                    <img id="img_consulta" src="_img/formulario_consultar_chamado.png"
+                                        alt="formulario_consultar_chamado.png">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
